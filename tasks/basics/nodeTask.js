b26ed7f38f9df383c79bba302b75d89dbfe09c2f
KHYehor <voodo1203@gmail.com>
2018-10-11 19:23:04 +0300

    Remade constructor

--- tasks/basics/nodeTask.js
+++ tasks/basics/nodeTask.js
@@ -3,34 +3,34 @@
 const fs = require('fs');
 
 const collection = {
-  sort: arr => arr.sort((a, b) => a - b),
-  filter: arr => arr.filter(el => el > 10),
-  shift: arr => arr.shift(),
-  pop: arr => arr.pop()
+  sort: (arg1, arg2) => {
+    if (arg1 > arg2) return 1;
+    if (arg1 < arg2) return -1;
+    if (arg1 === arg2) return 0;
+  },
+  shift: true,
+  pop: true
 };
 
-const numbers = [4, 7, 12, 16, 90, 0, 567, 123, 657];
-
 class ArrayAdvanced extends Array {
-  constructor(array) {
-    super();
-    this.array = array.slice();
+  constructor(...elements) {
+    super(...elements);
   }
   specialSort(collection) {
-    if (collection.hasOwnProperty('sort')) collection.sort(this.array);
-    if (collection.hasOwnProperty('filter')) this.array = collection.filter(this.array);
-    if (collection.hasOwnProperty('shift')) collection.shift(this.array);
-    if (collection.hasOwnProperty('pop')) collection.pop(this.array);
-    return this.array;
+    if (collection.sort) this.sort(collection.sort);
+    if (collection.shift) this.shift();
+    if (collection.pop) this.pop();
+    return this;
   }
   log() {
-    fs.writeFile('log.json', JSON.stringify(this.array), 'utf-8', err => {
+    fs.writeFile('log.json', JSON.stringify(this), 'utf-8', err => {
       if (err) throw err;
-      console.log('Array logged to log.son file.');
+      console.log(`Array logged to log.son file: ${this}`);
     })
   }
 }
 
-const filtred = new ArrayAdvanced(numbers);
+const filtred = new ArrayAdvanced(4, 7, 12, 16, 90, 0, 567, 400, 123, 657);
 filtred.specialSort(collection);
 filtred.log();
+
