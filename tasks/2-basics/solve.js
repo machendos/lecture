e9d6317098e8d0b3e1a82450c08a308139f3da56
Bohdan <Brooko@localhost.localdomain>
2018-10-15 12:29:14 +0300

    Added function wrapper and method wrapper

--- tasks/2-basics/solve.js
+++ tasks/2-basics/solve.js
@@ -1,5 +1,11 @@
 'use strict';
 
+module.exports.wrapper = fn => (arr, obj) => {
+  const res = fn(arr, obj);
+  console.log(`Transformed array: [${arr.join(', ')}]`);
+  return res;
+};
+
 module.exports.unsideTransform = function(arr, obj) {
   let res = [];
   arr = arr.slice(0);
@@ -25,56 +31,60 @@ module.exports.sideTransform = function(arr, obj) {
   return res;
 };
 
-// function AdvancedArray(...args) {
-//   Array.apply(this, args);
-// }
+class AdvancedArray extends Array {
+  wrapper(method) {
+    const wrapped = function(obj) {
+      const res = method.call(this, obj);
+      console.log(`Transformed array: [${this.join(', ')}]`);
+      return res;
+    };
+    return wrapped.bind(this);
+  }
 
-// Object.setPrototypeOf(AdvancedArray.prototype, Array.prototype);
+  unsideTransform(obj) {
+    let res = [];
+    let arr = this;
+    arr = arr.slice(0);
+    if (obj.sort) arr = arr.sort(obj.sort);
+    if (obj.filter) arr = arr.filter(obj.filter);
+    if (obj.shift) res.unshift(arr.shift());
+    if (obj.pop) res.push(arr.pop());
+    res = res.filter(el =>  el !== undefined);
+    return res;
+  }
 
-class AdvancedArray extends Array {}
+  sideTransform(obj) {
+    let res = [];
+    let arr = this;
+    if (obj.sort) arr = arr.sort(obj.sort);
+    if (obj.filter) {
+      arr.forEach((element, i) => {
+        if (!obj.filter(element)) arr.splice(i, 1);
+      });
+    }
+    if (obj.shift) res.unshift(arr.shift());
+    if (obj.pop) res.push(arr.pop());
+    res = res.filter(el => el !== undefined);
+    return res;
+  }
+}
 
 module.exports.AdvancedArray = AdvancedArray;
-
-AdvancedArray.prototype.unsideTransform = function(obj) {
-  let res = [];
-  let arr = this;
-  arr = arr.slice(0);
-  if (obj.sort) arr = arr.sort(obj.sort);
-  if (obj.filter) arr = arr.filter(obj.filter);
-  if (obj.shift) res.unshift(arr.shift());
-  if (obj.pop) res.push(arr.pop());
-  res = res.filter(el =>  el !== undefined);
-  return res;
-};
 
 module.exports.AdvancedArray.prototype.unsideTransform = AdvancedArray
   .prototype.unsideTransform;
 
-AdvancedArray.prototype.sideTransform = function(obj) {
-  let res = [];
-  let arr = this;
-  if (obj.sort) arr = arr.sort(obj.sort);
-  if (obj.filter) {
-    arr.forEach((element, i) => {
-      if (!obj.filter(element)) arr.splice(i, 1);
-    });
-  }
-  if (obj.shift) res.unshift(arr.shift());
-  if (obj.pop) res.push(arr.pop());
-  res = res.filter(el => el !== undefined);
-  return res;
-};
-
 module.exports.AdvancedArray.prototype.sideTransform = AdvancedArray
   .prototype.sideTransform;
 
-// const arr = new AdvancedArray(1, 2, 3);
-// console.dir(arr.toString());
-
-// const arre = new Array(1, 2, 3);
-// console.dir(arre.toString());
-
-// console.log(Object.prototype.toString(arr));
-// console.log(Object.prototype.toString(arre));
-
-// console.log(Array.isArray(arr));
+// const arr = new AdvancedArray(1, 2, 3, 4, 5);
+// const options = {
+//   sort: () => 1,
+//   filter: el => el,
+//   pop: true,
+//   shift: true
+// };
+// const wrap = arr.wrapper(arr.unsideTransform)(options);
+// const res = wrap(options);
+// console.log(res);
+// console.log(arr);
